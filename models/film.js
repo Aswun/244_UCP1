df04f86f40fa0b269465c146cba179a2da777f23
Aswun <aswinlutfian32@gmail.com>
2025-11-04 09:44:22 +0700

    minor fix

--- models/film.js
+++ models/film.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING
         },
         deskripsi: {
-            type: DataTypes.TEXT
+            type: DataTypes.STRING
         },
         sutradara: {
             type: DataTypes.STRING
